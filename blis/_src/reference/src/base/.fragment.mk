489ad4f3c7202942f8d6ef6efdec70126e69dfa1
Matthew Honnibal <honnibal+gh@gmail.com>
2017-10-15 12:38:45 +0200

    Add reference files, to avoid building on CI

---
+++ blis/_src/reference/src/base/.fragment.mk
@@ -0,0 +1,73 @@
+#
+#
+#  BLIS    
+#  An object-based framework for developing high-performance BLAS-like
+#  libraries.
+#
+#  Copyright (C) 2014, The University of Texas at Austin
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are
+#  met:
+#   - Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#   - Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#   - Neither the name of The University of Texas at Austin nor the names
+#     of its contributors may be used to endorse or promote products
+#     derived from this software without specific prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+#  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+#  HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+#  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+#  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+#  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+#
+
+#
+# fragment.mk 
+#
+# This is an automatically-generated makefile fragment and will likely get
+# overwritten or deleted if the user is not careful. Modify at your own risk.
+#
+
+# These two mmakefile variables need to be set in order for the recursive
+# include process to work!
+CURRENT_DIR_NAME := base
+CURRENT_SUB_DIRS := check noopt
+
+# Source files local to this fragment
+LOCAL_SRC_FILES  := bli_blksz.c bli_check.c bli_clock.c bli_cntl.c bli_cntx.c bli_const.c bli_error.c bli_func.c bli_getopt.c bli_gks.c bli_info.c bli_init.c bli_machval.c bli_malloc.c bli_mbool.c bli_membrk.c bli_memsys.c bli_obj.c bli_obj_scalar.c bli_opid.c bli_param_map.c bli_part.c bli_pool.c bli_prune.c bli_query.c
+
+# Add the fragment's local source files to the _global_variable_ variable.
+MK_FRAME_SRC += $(addprefix $(PARENT_PATH)/$(CURRENT_DIR_NAME)/, $(LOCAL_SRC_FILES))
+
+
+
+
+# -----------------------------------------------------------------------------
+# NOTE: The code below is generic and should remain in all fragment.mk files!
+# -----------------------------------------------------------------------------
+
+# Add the current fragment to the global list of fragments so the top-level
+# Makefile knows which directories are participating in the build.
+FRAGMENT_DIR_PATHS  += $(PARENT_PATH)/$(CURRENT_DIR_NAME)
+
+# Recursively descend into other subfragments' local makefiles and include them.
+ifneq ($(strip $(CURRENT_SUB_DIRS)),)
+key                 := $(key).x
+stack_$(key)        := $(PARENT_PATH)
+PARENT_PATH         := $(PARENT_PATH)/$(CURRENT_DIR_NAME)
+FRAGMENT_SUB_DIRS   := $(addprefix $(PARENT_PATH)/, $(CURRENT_SUB_DIRS))
+-include  $(addsuffix /$(FRAGMENT_MK), $(FRAGMENT_SUB_DIRS))
+PARENT_PATH         := $(stack_$(key))
+key                 := $(basename $(key))
+endif
