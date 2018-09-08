a1748129df5b8d14d96695a4dc0d4529cd3b28d1
Matthew Honnibal <honnibal+gh@gmail.com>
2018-09-08 17:15:31 +0200

    Fix include paths for Windows

--- setup.py
+++ setup.py
@@ -134,6 +134,7 @@ class ExtensionBuilder(distutils.command.build_ext.build_ext):
                 if py_compiler == 'msvc':
                     target_name = target_name.replace('/', '\\\\')
                     spec['source'] = spec['source'].replace('/', '\\\\')
+                    spec['include'] = [inc.replace('/', '\\\\') for inc in spec['include']]
                 spec['target'] = os.path.join(obj_dir, target_name)
                 spec['source'] = os.path.join(BLIS_DIR, spec['source'])
                 objects.append(self.build_object(env=env, **spec))
