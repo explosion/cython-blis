b777a3d626937d3bb3933784036e039713e5735c
Matthew Honnibal <honnibal+gh@gmail.com>
2018-10-06 23:56:49 +0200

    Try -0

--- setup.py
+++ setup.py
@@ -104,7 +104,7 @@ class ExtensionBuilder(distutils.command.build_ext.build_ext, build_ext_options)
             e.include_dirs.append(numpy.get_include())
             e.include_dirs.append(
                 os.path.join(INCLUDE, '%s-%s' % (platform_name, arch)))
-            e.extra_objects = list(objects)[:-200]
+            e.extra_objects = list(objects)
         distutils.command.build_ext.build_ext.build_extensions(self)
     
     def get_arch_name(self):
