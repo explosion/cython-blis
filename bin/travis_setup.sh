a3914d0982a395782d016d1aff38872bb6ba2b91
Matthew Honnibal <honnibal+gh@gmail.com>
2018-10-13 23:16:05 +0200

    Set CC env var for travis

--- bin/travis_setup.sh
+++ bin/travis_setup.sh
@@ -5,3 +5,5 @@ sudo ln -s /usr/lib/binutils-2.26/bin/as /usr/bin/as
 sudo rm -f /usr/bin/ld
 sudo ln -s /usr/lib/binutils-2.26/bin/ld /usr/bin/ld
 sed -i 's/"gcc"/"gcc-6"/' blis/_src/make/linux-x86_64.jsonl
+
+export CC="gcc-6"; fi
