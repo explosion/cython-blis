765c52cd0391953a3d4b04041d0fe75cf736c01c
Matthew Honnibal <honnibal+gh@gmail.com>
2018-10-13 23:21:44 +0200

    Try to update apt in travis setup

--- bin/travis_setup.sh
+++ bin/travis_setup.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+sudo apt-get update -y
+
 sudo rm -f /usr/bin/as
 sudo ln -s /usr/lib/binutils-2.26/bin/as /usr/bin/as
 sudo rm -f /usr/bin/ld
 sudo ln -s /usr/lib/binutils-2.26/bin/ld /usr/bin/ld
 sed -i 's/"gcc"/"gcc-6"/' blis/_src/make/linux-x86_64.jsonl
 
-export CC="gcc-6"; fi
+export CC="gcc-6"
