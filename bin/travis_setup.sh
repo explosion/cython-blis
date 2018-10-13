2b887fa60d92e62d1715f4b9cbedbac1cad987a4
Matthew Honnibal <honnibal+gh@gmail.com>
2018-10-13 20:00:04 +0200

    Add travis_setup.sh

---
+++ bin/travis_setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+sudo rm -f /usr/bin/as
+sudo ln -s /usr/lib/binutils-2.26/bin/as /usr/bin/as
+sudo rm -f /usr/bin/ld
+sudo ln -s /usr/lib/binutils-2.26/bin/ld /usr/bin/ld
+sed -i 's/"gcc"/"gcc-6"/' blis/_src/make/linux-x86_64.jsonl
