669d875b37c3e6911037e749e7e8f204b60e152f
Matthew Honnibal <honnibal+gh@gmail.com>
2018-10-20 17:20:19 +0200

    Prevent race condition in pacakge update

--- bin/travis_setup.sh
+++ bin/travis_setup.sh
@@ -7,6 +7,7 @@ if [ "$TRAVIS_OS_NAME" = "linux" ]; then
   sudo -E apt-add-repository -y "ppa:ubuntu-toolchain-r/test"
   sleep 5
   sudo apt-get update -y
+  sleep 5
   sudo apt-get install -y gcc-6 binutils clang
   sed -i 's/"gcc"/"gcc-6"/' blis/_src/make/linux-x86_64.jsonl
   export CC="gcc-6"
