52311771517ac17ec967e5c6b8dd17db03c6ccd4
Matthew Honnibal <honnibal+gh@gmail.com>
2019-02-27 17:43:33 +0100

    Set version to v0.2.3

--- blis/about.py
+++ blis/about.py
@@ -4,7 +4,7 @@
 # https://github.com/pypa/warehouse/blob/master/warehouse/__about__.py
 
 __name__ = 'blis'
-__version__ = '0.2.3.dev3'
+__version__ = '0.2.3'
 __summary__ = "Practical Machine Learning for NLP"
 __summary__ = "The Blis BLAS-like linear algebra library, as a self-contained C-extension.",
 __uri__ = 'https://github.com/explosion/cython-blis'
