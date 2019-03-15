6e70b539039293e67507ce5f055b177b35d74715
Matthew Honnibal <honnibal+gh@gmail.com>
2019-03-15 01:20:46 +0100

    Add buildkite scripts

---
+++ fabfile.py
@@ -0,0 +1,82 @@
+# coding: utf-8
+from __future__ import unicode_literals, print_function
+
+import contextlib
+from pathlib import Path
+from fabric.api import local, lcd, env, settings, prefix
+from os import path, environ
+import shutil
+import sys
+
+
+PWD = path.dirname(__file__)
+ENV = environ['VENV_DIR'] if 'VENV_DIR' in environ else '.env'
+VENV_DIR = Path(PWD) / ENV
+
+
+@contextlib.contextmanager
+def virtualenv(name, create=False, python='/usr/bin/python3.6'):
+    python = Path(python).resolve()
+    env_path = VENV_DIR
+    if create:
+        if env_path.exists():
+            shutil.rmtree(str(env_path))
+        local('{python} -m venv {env_path}'.format(python=python, env_path=VENV_DIR))
+    def wrapped_local(cmd, env_vars=[], capture=False, direct=False):
+        return local('source {}/bin/activate && {}'.format(env_path, cmd),
+                     shell='/bin/bash', capture=False)
+    yield wrapped_local
+
+
+def env(lang='python3.6'):
+    if VENV_DIR.exists():
+        local('rm -rf {env}'.format(env=VENV_DIR))
+    if lang.startswith('python3'):
+        local('{lang} -m venv {env}'.format(lang=lang, env=VENV_DIR))
+    else:
+        local('{lang} -m pip install virtualenv --no-cache-dir'.format(lang=lang))
+        local('{lang} -m virtualenv {env} --no-cache-dir'.format(lang=lang, env=VENV_DIR))
+    with virtualenv(VENV_DIR) as venv_local:
+        print(venv_local('python --version', capture=True))
+        venv_local('pip install --upgrade setuptools --no-cache-dir')
+        venv_local('pip install pytest --no-cache-dir')
+        venv_local('pip install wheel --no-cache-dir')
+        venv_local('pip install -r requirements.txt --no-cache-dir')
+        venv_local('pip install pex --no-cache-dir')
+
+
+
+def install():
+    with virtualenv(VENV_DIR) as venv_local:
+        venv_local('pip install dist/*.tar.gz')
+
+
+def make():
+    with lcd(path.dirname(__file__)):
+        local('export PYTHONPATH=`pwd` && source .env/bin/activate && python setup.py build_ext --inplace',
+            shell='/bin/bash')
+
+def sdist():
+    with virtualenv(VENV_DIR) as venv_local:
+        with lcd(path.dirname(__file__)):
+            local('python -m pip install -U setuptools')
+            local('python setup.py sdist')
+
+def wheel():
+    with virtualenv(VENV_DIR) as venv_local:
+        with lcd(path.dirname(__file__)):
+            venv_local('python setup.py bdist_wheel')
+
+
+def clean():
+    with lcd(path.dirname(__file__)):
+        local('rm -f dist/*.whl')
+        local('rm -f dist/*.pex')
+        with virtualenv(VENV_DIR) as venv_local:
+            venv_local('python setup.py clean --all')
+
+
+def test():
+    with virtualenv(VENV_DIR) as venv_local:
+        with lcd(path.dirname(__file__)):
+            venv_local('pytest -x tests')
