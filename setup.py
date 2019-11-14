8a4d1874030bec7ff056fc78a365ffc918d68fa1
Ines Montani <ines@ines.io>
2019-11-14 22:06:53 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -199,7 +199,10 @@ class ExtensionBuilder(distutils.command.build_ext.build_ext, build_ext_options)
         command.extend(include)
         print("[COMMAND]", " ".join(command))
         try:
-            subprocess.run(command, cwd=BLIS_DIR, check=True, capture_output=True)
+            kwargs = {"cwd": BLIS_DIR, "check": True}
+            if sys.version_info[0] == 3 and sys.version_info[1] > 6:
+                kwargs["capture_output"] = True
+            subprocess.run(command, **kwargs)
         except Exception as err:
             raise ValueError(getattr(err, "stderr", err))
         return target
