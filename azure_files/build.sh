b944a669cc406777555d4847c976a282cfce16c4
Matthew Honnibal <honnibal+gh@gmail.com>
2019-11-14 14:10:53 +0100

    Try adding conda-forge scripts

---
+++ azure_files/build.sh
@@ -0,0 +1,22 @@
+export PATH="$PREFIX/Library/bin:$BUILD_PREFIX/Library/bin:$PATH"
+export CC=clang
+export RANLIB=echo
+export LIBPTHREAD=
+export AS=llvm-as
+export AR=llvm-ar
+export CFLAGS="-MD -I$PREFIX/Library/include"
+export LDFLAGS="$LDFLAGS -L$PREFIX/Library/lib"
+clang --version
+llvm-as --version
+llvm-ar --version
+./configure --disable-shared --enable-static --prefix=$PREFIX/Library --enable-cblas --enable-threading=pthreads --enable-arg-max-hack x86_64
+make -j${CPU_COUNT}
+make install
+make check -j${CPU_COUNT}
+ 
+./configure --enable-shared --disable-static --prefix=$PREFIX/Library --enable-cblas --enable-threading=pthreads --enable-arg-max-hack x86_64
+make -j${CPU_COUNT}
+make install
+mv $PREFIX/Library/lib/libblis.lib $PREFIX/Library/lib/blis.lib
+mv $PREFIX/Library/lib/libblis.a $PREFIX/Library/lib/libblis.lib
+mv $PREFIX/Library/lib/libblis.*.dll $PREFIX/Library/bin/
