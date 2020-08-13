14a2b353bbaec86da3e6964582a117408f2d1b89
Matthew Honnibal <honnibal+gh@gmail.com>
2020-08-13 23:37:46 +0200

    Update compiler settings

--- blis/_src/config/x86_64_no_skx/make_defs.mk
+++ blis/_src/config/x86_64_no_skx/make_defs.mk
@@ -79,10 +79,16 @@ endif
 # Flags specific to reference kernels.
 CROPTFLAGS     := $(CKOPTFLAGS)
 ifeq ($(CC_VENDOR),gcc)
-CRVECFLAGS     := $(CKVECFLAGS)
+CRVECFLAGS     := $(CKVECFLAGS) -funsafe-math-optimizations -ffp-contract=fast
+else
+ifeq ($(CC_VENDOR),clang)
+CRVECFLAGS     := $(CKVECFLAGS) -funsafe-math-optimizations -ffp-contract=fast
 else
 CRVECFLAGS     := $(CKVECFLAGS)
 endif
+endif
+
+
 
 # Store all of the variables here to new variables containing the
 # configuration name.
