35823f2d9f9f35946377b04add36485d6ecec36f
Adriane Boyd <adrianeboyd@gmail.com>
2020-11-25 13:49:03 +0100

    Set version to v0.4.3

--- blis/about.py
+++ blis/about.py
@@ -5,7 +5,7 @@
 # https://github.com/pypa/warehouse/blob/master/warehouse/__about__.py
 
 __name__ = "blis"
-__version__ = "0.4.2"
+__version__ = "0.4.3"
 __summary__ = (
     "The Blis BLAS-like linear algebra library, as a self-contained C-extension."
 )
