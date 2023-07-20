6b1a6881350bc8339d9d7c7f95069b9c99b5d029
Adriane Boyd <adrianeboyd@gmail.com>
2023-07-20 11:43:53 +0200

    Update numpy build constraints for numpy 1.25 (#90)
    Starting in numpy 1.25 (see
    https://github.com/numpy/numpy/releases/tag/v1.25.0), the numpy C API is
    backwards-compatible by default.
    
    For python 3.9+, we should be able to drop the specific numpy build
    requirements and use `numpy>=1.25`, which is currently
    backwards-compatible to `numpy>=1.19`.
    
    In the future, the python <3.9 requirements could be dropped and the
    lower numpy pin could correspond to the oldest supported version for the
    current lower python pin.

--- setup.py
+++ setup.py
@@ -299,7 +299,10 @@ setup(
         "cython>=0.25",
         "numpy>=1.15.0",
     ],
-    install_requires=["numpy>=1.15.0"],
+    install_requires=[
+        "numpy>=1.15.0; python_version < '3.9'",
+        "numpy>=1.19.0; python_version >= '3.9'",
+    ],
     ext_modules=cythonize([
         Extension(
             "blis.cy", [os.path.join("blis", "cy.pyx")], extra_compile_args=["-std=c99"]
