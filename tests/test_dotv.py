2637f3f9f07eb0b2afa2c7a895ced4ae1147a469
Matthew Honnibal <honnibal+gh@gmail.com>
2024-07-18 12:14:05 +0200

    Try to add more debugging

--- tests/test_dotv.py
+++ tests/test_dotv.py
@@ -1,10 +1,9 @@
 from __future__ import division
-from hypothesis import given, assume, reproduce_failure
-from math import sqrt, floor
+from hypothesis import given, assume
+import numpy
 
 from blis_tests_common import *
 from blis.py import dotv
-from blis.cy import NO_CONJUGATE, CONJUGATE
 
 
 @given(
@@ -23,12 +22,6 @@ def test_memoryview_double_noconj(A, B):
     assert_allclose([numpy_result], result, atol=1e-3, rtol=1e-3)
 
 
-@reproduce_failure(
-    "6.103.5", b"AXicY2AAggaXUAauVetzGBiBHMYGBjBgZATyGCFshhRGJgY4SGFkBmmCKmNAAgAMFQS/"
-)
-@reproduce_failure(
-    "6.103.5", b"AXicE2NAAGsxRiDJCOUpMzIipJQZWYDkQY6POYVSFz8h6WEAAGinBSs="
-)
 @given(
     ndarrays(min_len=10, max_len=100, min_val=-100.0, max_val=100.0, dtype="float32"),
     ndarrays(min_len=10, max_len=100, min_val=-100.0, max_val=100.0, dtype="float32"),
@@ -42,4 +35,19 @@ def test_memoryview_float_noconj(A, B):
     assume(B is not None)
     numpy_result = A.dot(B)
     result = dotv(A, B)
-    assert_allclose([numpy_result], result, atol=1e-2, rtol=1e-2)
+    # We want to also know the true(r) answer, if one of them is off.
+    A_float64 = A.astype(numpy.float64)
+    B_float64 = B.astype(numpy.float64)
+    numpy_result64 = A_float64.dot(B_float64)
+    blis_result64 = dotv(A_float64, B_float64)
+    try:
+        assert_allclose(
+            [numpy_result],
+            result,
+            atol=1e-3,
+            rtol=1e-3,
+        )
+    except AssertionError as e:
+        print(f"Numpy 64bit result: {numpy_result64}")
+        print(f"blis 64bit result: {blis_result64}")
+        raise e
