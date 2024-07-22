a5bdba194c010731049e70059f6d9f2ed44dd936
Matthew Honnibal <honnibal+gh@gmail.com>
2024-07-22 09:46:57 +0200

    v1.0.0: Update numpy pins (#109)
    * Update pins and bump version
    
    * Remove old pythons
    
    * Remove Python3.9
    
    * Set macos 13
    
    * Reenable 3.9
    
    * Fix pyproject
    
    * Fix build constraint
    
    * Wip/cibuildwheel (#110)
    
    * Try to add cibuildwheel workflow
    
    * Configure cibuildwheel
    
    * Remove macos 14
    
    * Fix workflow
    
    * Update cibuildwheel config
    
    * Try to fix archs commands
    
    * Fix config
    
    * Skip pypy
    
    * Add another failure repro decorator
    
    * Temporarily try increasing error tolerance
    
    * Try to add more debugging
    
    * Comment
    
    * Try to get cibuildwheel uploading to pypi

--- tests/test_dotv.py
+++ tests/test_dotv.py
@@ -1,23 +1,20 @@
 from __future__ import division
 from hypothesis import given, assume
-from math import sqrt, floor
+import numpy
 
 from blis_tests_common import *
 from blis.py import dotv
-from blis.cy import NO_CONJUGATE, CONJUGATE
 
 
 @given(
-    ndarrays(min_len=10, max_len=100,
-             min_val=-100.0, max_val=100.0, dtype='float64'),
-    ndarrays(min_len=10, max_len=100,
-             min_val=-100.0, max_val=100.0, dtype='float64'),
+    ndarrays(min_len=10, max_len=100, min_val=-100.0, max_val=100.0, dtype="float64"),
+    ndarrays(min_len=10, max_len=100, min_val=-100.0, max_val=100.0, dtype="float64"),
 )
 def test_memoryview_double_noconj(A, B):
     if len(A) < len(B):
-        B = B[:len(A)]
+        B = B[: len(A)]
     else:
-        A = A[:len(B)]
+        A = A[: len(B)]
     assume(A is not None)
     assume(B is not None)
     numpy_result = A.dot(B)
@@ -26,18 +23,32 @@ def test_memoryview_double_noconj(A, B):
 
 
 @given(
-    ndarrays(min_len=10, max_len=100,
-             min_val=-100.0, max_val=100.0, dtype='float32'),
-    ndarrays(min_len=10, max_len=100,
-             min_val=-100.0, max_val=100.0, dtype='float32'),
+    ndarrays(min_len=10, max_len=100, min_val=-100.0, max_val=100.0, dtype="float32"),
+    ndarrays(min_len=10, max_len=100, min_val=-100.0, max_val=100.0, dtype="float32"),
 )
 def test_memoryview_float_noconj(A, B):
     if len(A) < len(B):
-        B = B[:len(A)]
+        B = B[: len(A)]
     else:
-        A = A[:len(B)]
+        A = A[: len(B)]
     assume(A is not None)
     assume(B is not None)
     numpy_result = A.dot(B)
     result = dotv(A, B)
-    assert_allclose([numpy_result], result, atol=1e-3, rtol=1e-3)
+    # We want to also know the true(r) answer, if one of them is off.
+    A_float64 = A.astype(numpy.float64)
+    B_float64 = B.astype(numpy.float64)
+    numpy_result64 = A_float64.dot(B_float64)
+    blis_result64 = dotv(A_float64, B_float64)
+    try:
+        assert_allclose(
+            [numpy_result],
+            result,
+            atol=1e-3,
+            rtol=1e-3,
+        )
+    except AssertionError as e:
+        # Probably better to make a combined message, but eh
+        print(f"Numpy 64bit result: {numpy_result64}")
+        print(f"blis 64bit result: {blis_result64}")
+        raise e
