1270b2b1e6c2f9f20abdac146febc3201e32e91d
Lysandros Nikolaou <lisandrosnik@gmail.com>
2025-09-30 15:26:43 +0200

    Apply suggestion from @crusaderky
    Co-authored-by: Guido Imperiale <crusaderky@gmail.com>

--- setup.py
+++ setup.py
@@ -324,7 +324,7 @@ setup(
         ],
         language_level=2,
     ),
-    python_requires=">=3.6,<3.15",
+    python_requires=">=3.10,<3.15",
     cmdclass={"build_ext": ExtensionBuilder},
     package_data={"": ["*.json", "*.jsonl", "*.pyx", "*.pxd"]},
     name="blis",
