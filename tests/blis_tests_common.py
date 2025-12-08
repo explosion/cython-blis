5c90b6938becbb97e8df7110e01ae0a746414bb5
crusaderky <crusaderky@gmail.com>
2025-12-08 17:41:37 +0000

    Overaul hypothesis strategies
    Improve readability. 
    Given infinite hypothesis examples, this commit does not introduce any functional changes.
    However, given a fixed and relatively low max_examples setting, it substantially increases test coverage:
    
    1. It prevents examples that were previously skipped by assume(). According to pytest --hypothesis-show-statistics, these were ~10% for dotv and ~20% for gemm.
    
    2. It prevents examples that ended up being duplicates due to trimming. For example, in dotv, hypothesis could previously generate examples e.g. A=[1,2,3,4], B=[5,6] and A=[1,2], B=[5,6,7,8]; both would get trimmed to A=[1,2], B=[5,6].
    
    3. In gemm, it removes an entire degree of freedom by removing unused variable out_col, which again would result in duplicate examples.

--- tests/blis_tests_common.py
+++ tests/blis_tests_common.py
@@ -1,34 +1,25 @@
 # Copyright ExplosionAI GmbH, released under BSD.
-import numpy as np
-
-np.random.seed(0)
-
 from hypothesis import settings
-from hypothesis.strategies import integers, floats
+from hypothesis import strategies as st
 from hypothesis.extra.numpy import arrays
 
 # Increase this to run more thorough tests
-hypothesis_default_profile = settings.register_profile("default", max_examples=100)
+hypothesis_default_profile = settings.register_profile("default", max_examples=200)
+
+# (dtype, atol, rtol)
+dtypes_tols = st.sampled_from(
+    [
+        # FIXME huge tolerance needed for float32:
+        # https://github.com/explosion/cython-blis/issues/142
+        ("float32", 1e-2, 1e-4),
+        ("float64", 1e-9, 1e-9),
+    ]
+)
 
 
-def lengths(lo, hi):
-    return integers(min_value=lo, max_value=hi)
-
-
-def ndarrays_of_shape(shape, lo, hi, dtype):
-    width = 64 if dtype == "float64" else 32
-    return arrays(
-        dtype,
-        shape=shape,
-        elements=floats(
-            min_value=lo,
-            max_value=hi,
-            width=width,
-        ),
-    )
-
-
-def ndarrays(min_len, max_len, min_val, max_val, dtype):
-    return lengths(lo=min_len, hi=max_len).flatmap(
-        lambda n: ndarrays_of_shape(n, lo=min_val, hi=max_val, dtype=dtype)
-    )
+def ndarrays(shape, lo, hi, dtype):
+    """Draw ND NumPy arrays of floats"""
+    assert isinstance(dtype, str) and dtype.startswith("float")
+    width = int(dtype[5:])
+    elements = st.floats(min_value=lo, max_value=hi, width=width)
+    return arrays(dtype, shape=shape, elements=elements)
--- tests/test_dotv.py
+++ tests/test_dotv.py
@@ -1,11 +1,21 @@
 # Copyright ExplosionAI GmbH, released under BSD.
 import numpy as np
 import pytest
-from hypothesis import given, assume
-
+from hypothesis import given
+from hypothesis import strategies as st
 from numpy.testing import assert_allclose
-from blis_tests_common import ndarrays
+
+from blis_tests_common import dtypes_tols, ndarrays
 from blis.py import dotv
+
+
+@st.composite
+def dotv_arrays(draw):
+    """Draw two 1D NumPy arrays of the same size and dtype"""
+    size = draw(st.integers(min_value=1, max_value=100))
+    dtype, atol, rtol = draw(dtypes_tols)
+    arr_st = ndarrays((size,), lo=-100.0, hi=100.0, dtype=dtype)
+    return draw(arr_st), draw(arr_st), atol, rtol
 
 
 def test_incompatible_shape():
@@ -13,33 +23,9 @@ def test_incompatible_shape():
         dotv(np.zeros(2), np.zeros(3))
 
 
-@given(
-    ndarrays(min_len=1, max_len=100, min_val=-100.0, max_val=100.0, dtype="float64"),
-    ndarrays(min_len=1, max_len=100, min_val=-100.0, max_val=100.0, dtype="float64"),
-)
-def test_memoryview_double_noconj(A, B):
-    if len(A) < len(B):
-        B = B[: len(A)]
-    else:
-        A = A[: len(B)]
-    assume(A is not None)
-    assume(B is not None)
+@given(dotv_arrays())
+def test_memoryview_noconj(arrays):
+    A, B, atol, rtol = arrays
     numpy_result = A.dot(B)
     result = dotv(A, B)
-    assert_allclose(result, numpy_result, atol=1e-9, rtol=1e-9)
-
-
-@given(
-    ndarrays(min_len=1, max_len=100, min_val=-100.0, max_val=100.0, dtype="float32"),
-    ndarrays(min_len=1, max_len=100, min_val=-100.0, max_val=100.0, dtype="float32"),
-)
-def test_memoryview_float_noconj(A, B):
-    if len(A) < len(B):
-        B = B[: len(A)]
-    else:
-        A = A[: len(B)]
-    assume(A is not None)
-    assume(B is not None)
-    numpy_result = A.dot(B)
-    result = dotv(A, B)
-    assert_allclose(result, numpy_result, atol=1e-2, rtol=1e-4)
+    assert_allclose(result, numpy_result, atol=atol, rtol=rtol)
